0a857cef1c7bb7c22c6a317ac9efdec6ed90410f
Hritik Kumar Agarwal <agarwal.hritik225@gmail.com>
2024-10-12 09:35:10 +0530

    Added Functionality Dockers!

--- Employee_Management_System_using_TextFile_Storage/Employee_Management_System_File_Storage.py
+++ Employee_Management_System_using_TextFile_Storage/Employee_Management_System_File_Storage.py
@@ -1,46 +1,59 @@
 #Employee Management System using Permanent File Storage (Text File)
 #Employee Class
 class Employee:
-    def __init__(self):
-        ID=None
-        Name=None
-        Department=None
-        Salary=None
+    """ Employee Class to store the Employee Attributes
+    """
+    def __init__(self,ID,Name,Department,Salary) -> None:
+        self.ID: int=ID
+        self.Name: str=Name
+        self.Department: str=Department
+        self.Salary: int=Salary
 #Input Employee Attributes
-def InputRecord(emp: Employee):
-    emp.ID=int(input("Enter Employee ID :"))
-    emp.Name=input("Enter Employee Name :")
-    emp.Department=input("Enter Employee Department :")
-    emp.Salary=int(input("Enter Employee Salary :"))
+def InputRecord() -> Employee:
+    """InputRecord Function to take Employee Parameter input and store it in Employee Class
+
+    Returns:
+        Employee: Employe Class
+    """
+    ID=int(input("Enter Employee ID :"))
+    Name: str=input("Enter Employee Name :")
+    Department: str=input("Enter Employee Department :")
+    Salary=int(input("Enter Employee Salary :"))
+    emp= Employee(ID,Name,Department,Salary)
+    return emp
+
 #Inserting the Employee Attributes to Text File
-def InsertRecord():
-    Emp=Employee()
-    InputRecord(Emp)
+def InsertRecord() -> None:
+    """InsertRecord Function to Insert the Employee Paramters to Text File
+    """
+    Emp: Employee=InputRecord()
     with open("employee_details.txt","a") as fileobject:
-        outputstream="%d,%s,%s,%d\n"%(Emp.ID,Emp.Name,Emp.Department,Emp.Salary)
+        outputstream: str="%d,%s,%s,%d\n"%(Emp.ID,Emp.Name,Emp.Department,Emp.Salary)
         fileobject.write(outputstream)
     print("\nRecord Entry Added to Database!")
 #Display Records from the Text File
-def DisplayRecords():
+def DisplayRecords() -> None:
+    """DisplayRecords Function to Display the Employee Records stored in the Text File
+    """
     with open("employee_details.txt","r") as fileobject:
-        data=fileobject.readlines()
+        data: list[str]=fileobject.readlines()
         print("%-30s%-30s%-30s%-30s"%("ID","Name","Department","Salary"))
         print("----------"*12)
         for i in data:
-            entrylist=i.split(",")
+            entrylist: list[str]=i.split(",")
             for j in entrylist:
                 if(j.endswith("\n")):
-                    j=j[:len(j)-1]
+                    j: str=j[:len(j)-1]
                 print("%-30s"%(j),end="")
             print()
 #Search a Record from the Text File
-def SearchRecord():
+def SearchRecord() -> None:
     pass
-def UpdateRecord():
+def UpdateRecord() -> None:
     pass
-def DeleteRecord():
+def DeleteRecord() -> None:
     pass
-def main():
+def main() -> None:
     while True:
         print()
         print("----------"*12)
